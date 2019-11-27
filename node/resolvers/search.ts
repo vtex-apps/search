7e06cef66977af86ef7f38483c16c309a123c774
Christian Mutti <christian.mutti@vtex.com.br>
2019-11-27 15:14:53 -0300

    Restrict try-catch

--- node/resolvers/search.ts
+++ node/resolvers/search.ts
@@ -6,23 +6,9 @@ export const search = {
   searchResult: async (_: any, args: SearchResultInput, ctx: IContext) => {
     const { biggySearch } = ctx.clients;
 
+    let result: any = {};
     try {
-      const result = (await biggySearch.searchResult(args)) || {};
-      result.products = result.products || [];
-
-      result.products.forEach((product: any) => {
-        const mapInfo = product.extraInfo || {};
-        const extraInfo: { key: string; value: string }[] = [];
-
-        // Transform ExtraInfo from Map<String, String> to Array<KeyValueTuple>.
-        for (const key of Object.keys(mapInfo)) {
-          extraInfo.push({ key, value: mapInfo[key] });
-        }
-
-        product.extraInfo = extraInfo;
-      });
-
-      return result;
+      result = (await biggySearch.searchResult(args)) || {};
     } catch (err) {
       if (path(["response", "status"], err) === 302) {
         const redirect = path(["response", "headers", "location"], err);
@@ -31,5 +17,21 @@ export const search = {
 
       throw err;
     }
+
+    result.products = result.products || [];
+
+    result.products.forEach((product: any) => {
+      const mapInfo = product.extraInfo || {};
+      const extraInfo: { key: string; value: string }[] = [];
+
+      // Transform ExtraInfo from Map<String, String> to Array<KeyValueTuple>.
+      for (const key of Object.keys(mapInfo)) {
+        extraInfo.push({ key, value: mapInfo[key] });
+      }
+
+      product.extraInfo = extraInfo;
+    });
+
+    return result;
   },
 };
