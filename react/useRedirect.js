e4585cd210cd63ad6a68102c0740c57d2f789d8e
Christian Mutti <christian.mutti@vtex.com.br>
2019-11-27 10:25:39 -0300

    Change to location.replace so that history is not saved

--- react/useRedirect.js
+++ react/useRedirect.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from "react";
-import { useRuntime } from "vtex.render-runtime";
 
 const useRedirect = () => {
   const [redirect, setRedirect] = useState(null);
-  const { navigate } = useRuntime();
 
   useEffect(() => {
     if (redirect) {
@@ -11,11 +9,9 @@ const useRedirect = () => {
       const origin = window.location.origin.replace(/(http(s)?:)?\/\//, "");
 
       if (originRedirect.startsWith(origin)) {
-        navigate({
-          to: originRedirect.replace(origin, ""),
-        });
+        window.location.replace(originRedirect.replace(origin, ""));
       } else {
-        window.location.href = redirect;
+        window.location.replace(redirect);
       }
     }
   }, [redirect]);
