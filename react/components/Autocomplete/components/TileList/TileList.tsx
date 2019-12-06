c6536aa913d76793b3efd2d4c1d9812f8de2192a
Hiago Lucas Cardeal <hiago.cardeal@vtex.com>
2019-12-06 16:24:40 -0300

    change spinner color

--- react/components/Autocomplete/components/TileList/TileList.tsx
+++ react/components/Autocomplete/components/TileList/TileList.tsx
@@ -38,7 +38,7 @@ export class TileList extends React.Component<TileListProps> {
         ) : null}
         {this.props.isLoading ? (
           <div className={styles.tileListSpinner}>
-            <Spinner />
+            <Spinner color="#222222" />
           </div>
         ) : (
           <>
