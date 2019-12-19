42db06a825b8c9d2f9df8f1292d8197c08e2b772
Christian Mutti <christian.mutti@vtex.com.br>
2019-12-19 12:48:08 -0300

    Added temporary workaround on facet values sorting.

--- react/SearchContext.js
+++ react/SearchContext.js
@@ -19,7 +19,7 @@ const getUrlByAttributePath = (
     ? map
         .split(",")
         .slice(1)
-        .map((item, index) => `${item}/${facets[index]}`)
+        .map((item, index) => `${item}/${facets[index].replace(/^z/, "")}`)
     : [];
 
   const url = apiUrlTerms.join("/");
