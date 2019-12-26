6bc83759c221726b947a9effea09ec576793f5e2
Christian Mutti <christian.mutti@vtex.com.br>
2019-12-26 12:17:15 -0300

    PropTypes and convertOrderBy

--- react/SearchContext.js
+++ react/SearchContext.js
@@ -1,9 +1,10 @@
 import React, { useMemo } from "react";
+import PropTypes from "prop-types";
 import { prop } from "ramda";
-import { useRuntime } from "vtex.render-runtime";
+// import { useRuntime } from "vtex.render-runtime";
 import { SearchClickPixel } from "vtex.sae-analytics";
 
-import { vtexOrderToBiggyOrder } from "./utils/vtex-utils";
+import { convertOrderBy } from "./utils/compatibility-layer.ts";
 import useRedirect from "./useRedirect";
 import SearchQuery from "./components/SearchQuery";
 
@@ -32,7 +33,7 @@ const getUrlByAttributePath = (
 };
 
 const SearchContext = props => {
-  const { account } = useRuntime();
+  // const { account } = useRuntime();
   const { setRedirect } = useRedirect();
 
   const {
@@ -48,14 +49,14 @@ const SearchContext = props => {
     [attributePath, map, priceRange],
   );
 
-  const initialVariables = {
+  const variables = {
     operator,
     fuzzy,
     query: _query,
     page: 1,
     store: "exitocol", // TODO: remove
     attributePath: url,
-    sort: vtexOrderToBiggyOrder(order),
+    sort: convertOrderBy(order),
     count: maxItemsPerPage,
     leap: !!leap,
   };
@@ -67,7 +68,7 @@ const SearchContext = props => {
       priceRangeKey={priceRangeKey}
       attributePath={attributePath}
       map={map}
-      variables={initialVariables}
+      variables={variables}
     >
       {result => {
         const redirect = prop("redirect", result);
@@ -88,6 +89,20 @@ const SearchContext = props => {
       }}
     </SearchQuery>
   );
+};
+
+SearchContext.propTypes = {
+  children: PropTypes.arrayOf(PropTypes.node).isRequired,
+  priceRangeKey: PropTypes.string,
+  maxItemsPerPage: PropTypes.number,
+  params: PropTypes.shape({ path: PropTypes.string.isRequired }).isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  query: PropTypes.any.isRequired,
+};
+
+SearchContext.defaultProps = {
+  priceRangeKey: undefined,
+  maxItemsPerPage: 20,
 };
 
 export default SearchContext;
--- react/utils/compatibility-layer.ts
+++ react/utils/compatibility-layer.ts
@@ -95,3 +95,43 @@ export function fromAttributesToFacets(attribute: any) {
     }),
   };
 }
+
+type OrderBy =
+  | "OrderByPriceDESC"
+  | "OrderByPriceASC"
+  | "OrderByTopSaleDESC"
+  | "OrderByReviewRateDESC"
+  | "OrderByNameDESC"
+  | "OrderByNameASC"
+  | "OrderByReleaseDateDESC"
+  | "OrderByBestDiscountDESC";
+
+/**
+ * Convert from VTEX OrderBy into Biggy's sort.
+ *
+ * @export
+ * @param {OrderBy} orderBy VTEX OrderBy.
+ * @returns {string} Biggy's sort.
+ */
+export function convertOrderBy(orderBy: OrderBy): string {
+  switch (orderBy) {
+    case "OrderByPriceDESC":
+      return "price:desc";
+    case "OrderByPriceASC":
+      return "price:asc";
+    case "OrderByTopSaleDESC":
+      return "orders:desc";
+    case "OrderByReviewRateDESC":
+      return ""; // TODO: Not Supported
+    case "OrderByNameDESC":
+      return "name:desc";
+    case "OrderByNameASC":
+      return "name:asc";
+    case "OrderByReleaseDateDESC":
+      return "fields.release:desc";
+    case "OrderByBestDiscountDESC":
+      return ""; // TODO: Not Supported
+    default:
+      return "";
+  }
+}
