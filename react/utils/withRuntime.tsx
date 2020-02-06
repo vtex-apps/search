5149dc66537b2083d9afb67faea781392691b30d
Christian Mutti <34144667+chrsmutti@users.noreply.github.com>
2020-02-06 13:11:41 -0300

    Added OrderBy discount (#27)
    * Added orderby discount
    
    * Changelog

--- react/utils/withRuntime.tsx
+++ react/utils/withRuntime.tsx
@@ -1,6 +1,7 @@
 import { useRuntime } from "vtex.render-runtime";
 import React from "react";
 
+// tslint:disable-next-line:variable-name
 export function withRuntime(ComponentWrapped: typeof React.Component) {
   return function Wrapped(props: any) {
     return <ComponentWrapped {...props} runtime={useRuntime()} />;
