1be3f28a07b7ffdde4b807a1062948df23ce4ea5
Thalyta Fabrine <thalyta.trindade@vtex.com.br>
2020-11-30 11:17:27 -0300

    First test

---
+++ cypress/constants.js
@@ -0,0 +1,4 @@
+export const searchResultContainer = '.vtex-search-result-3-x-searchResultContainer'
+export const breadcrumb = '[data-testid=breadcrumb]'
+export const filtersWrapper = '.vtex-search-result-3-x-filtersWrapper'
+export const searchResultLoading = '.vtex-search-result-3-x-searchResultContainer > .vtex__icon-spinner'
