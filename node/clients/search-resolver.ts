62ffb6a3a63e44a805478d9261994ec0aac7ba9c
Hiago Lucas Cardeal de Melo Silva <hcardealdemelo@gmail.com>
2021-05-24 11:14:40 -0300

    add x-vtex-provider-header-and-update-dependencies

--- node/clients/search-resolver.ts
+++ node/clients/search-resolver.ts
@@ -3,7 +3,13 @@ import { pathOr } from "ramda";
 
 export class SearchResolver extends AppGraphQLClient {
   constructor(context: IOContext, options?: InstanceOptions) {
-    super("vtex.search-resolver", context, options);
+    super("vtex.search-resolver", context, {
+      ...options,
+      headers: {
+        ...(options?.headers ?? {}),
+        'x-vtex-provider': 'vtex.search-graphql'
+      },
+    });
   }
 
   public productsById = async (ids: string[]) => {
